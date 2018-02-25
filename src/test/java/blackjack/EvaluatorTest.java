2ca4c479e2358d1fbb0f55a4296fecc3b41b7493
Vivian Low <vivian198912@gmail.com>
2018-02-25 21:26:00 +0800

    EvaluatorTest - Test ACE value is one when hand count more than two

--- src/test/java/blackjack/EvaluatorTest.java
+++ src/test/java/blackjack/EvaluatorTest.java
@@ -23,7 +23,16 @@ public class EvaluatorTest {
         hand.draw(createSpadesCard(Rank.KING));
         Evaluator evaluator = new Evaluator();
         Assert.assertEquals(18, evaluator.computeScore(hand));
-        
+    }
+    
+    @Test
+    public void testComputeScoreAceValueIsOneWhenHandCountMoreThanTwo() {
+        Hand hand = new Hand();
+        hand.draw(createSpadesCard(Rank.ACE));
+        hand.draw(createSpadesCard(Rank.EIGHT));
+        hand.draw(createSpadesCard(Rank.KING));
+        Evaluator evaluator = new Evaluator();
+        Assert.assertEquals(19, evaluator.computeScore(hand));
     }
     
     private Card createSpadesCard(Rank rank) {
