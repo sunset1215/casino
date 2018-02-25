887d697dcc66997032a05950c63d55fe3257c3bd
Vivian Low <vivian198912@gmail.com>
2018-02-25 16:44:15 +0800

    BlackjackTest - add test player exceed 21 and lose

--- src/test/java/BlackjackTest.java
+++ src/test/java/BlackjackTest.java
@@ -12,24 +12,33 @@ public class BlackjackTest {
     }
     
     @Test
-    public void testPlayerBlackjackWin() {
+    public void testPlayerBlackjackAndWin() {
         game.setPlayerHand(new Hand(10, 11));
         game.setDealerHand(new Hand(10, 9));
         Assert.assertEquals("Player wins", Blackjack.Result.WIN, game.play());
     }
     
     @Test
-    public void testDealerBlackjackPlayerLose() {
+    public void testDealerBlackjackAndLose() {
         game.setPlayerHand(new Hand(10, 9));
         game.setDealerHand(new Hand(10, 11));
         Assert.assertEquals("Player loses", Blackjack.Result.LOSE, game.play());
     }
     
     @Test
-    public void testBothBlackjackPush() {
+    public void testBothBlackjackAndPush() {
         game.setPlayerHand(new Hand(10, 11));
         game.setDealerHand(new Hand(10, 11));
         Assert.assertEquals("Player pushes", Blackjack.Result.PUSH, game.play());
+    }
+    
+    @Test
+    public void testPlayerExceedAndLose() {
+        Hand playerHand = new Hand(10, 2);
+        playerHand.draw(10);
+        game.setPlayerHand(playerHand);
+        game.setDealerHand(new Hand(10, 11));
+        Assert.assertEquals("Player exceed and lose", Blackjack.Result.LOSE, game.play());
     }
 
 }
