23f952d36ebbab0c8837365053b7680011414849
Vivian Low <vivian198912@gmail.com>
2018-02-25 16:34:19 +0800

    Refactor

--- src/test/java/BlackjackTest.java
+++ src/test/java/BlackjackTest.java
@@ -1,25 +1,27 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BlackjackTest {
     
+    Blackjack game;
+    
+    @Before
+    public void setUp() {
+        game = new Blackjack();
+    }
+    
     @Test
     public void testPlayerBlackjackWin() {
-        Blackjack game = new Blackjack();
-        Hand playerHand = new Hand(10, 11);
-        Hand dealerHand = new Hand(10, 9);
-        game.setPlayerHand(playerHand);
-        game.setDealerHand(dealerHand);
+        game.setPlayerHand(new Hand(10, 11));
+        game.setDealerHand(new Hand(10, 9));
         Assert.assertEquals("Player wins", Blackjack.Result.WIN, game.play());
     }
     
     @Test
     public void testDealerBlackjackPlayerLose() {
-        Blackjack game = new Blackjack();
-        Hand playerHand = new Hand(10, 9);
-        Hand dealerHand = new Hand(10, 11);
-        game.setPlayerHand(playerHand);
-        game.setDealerHand(dealerHand);
+        game.setPlayerHand(new Hand(10, 9));
+        game.setDealerHand(new Hand(10, 11));
         Assert.assertEquals("Player loses", Blackjack.Result.LOSE, game.play());
     }
 
