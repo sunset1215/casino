4fe0d1af5bf8c623e11e429d4949dd05500399e2
Vivian Low <vivian198912@gmail.com>
2018-02-25 17:13:50 +0800

    BlackjackTest - add test both exceed and player keep

--- src/test/java/blackjack/BlackjackTest.java
+++ src/test/java/blackjack/BlackjackTest.java
@@ -52,6 +52,17 @@ public class BlackjackTest {
         Assert.assertEquals("Dealer exceed and player win", Blackjack.Result.WIN, game.play());
     }
     
+    @Test
+    public void testBothExceedAndKeep() {
+        Hand dealerHand = new Hand(10, 2);
+        dealerHand.draw(10);
+        Hand playerHand = new Hand(10, 2);
+        playerHand.draw(10);
+        game.setPlayerHand(playerHand);
+        game.setDealerHand(dealerHand);
+        Assert.assertEquals("Both exceed and player keep", Blackjack.Result.KEEP, game.play());
+    }
+    
     private Hand createBlackjackHand() {
         return new Hand(10, 11);
     }
