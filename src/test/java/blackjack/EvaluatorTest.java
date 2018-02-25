86054669245d5fce404de7b1a58b7b094a69ef89
Vivian Low <vivian198912@gmail.com>
2018-02-25 21:39:29 +0800

    Pass

--- src/test/java/blackjack/EvaluatorTest.java
+++ src/test/java/blackjack/EvaluatorTest.java
@@ -3,35 +3,44 @@ package blackjack;
 import enums.Rank;
 import enums.Suit;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class EvaluatorTest {
     
+    private static Evaluator evaluator;
+    private Hand hand;
+    
+    @BeforeClass
+    public static void initEvaluator() {
+        evaluator = new Evaluator();
+    }
+    
+    @Before
+    public void setUp() {
+        hand = new Hand();
+    }
+    
     @Test
     public void testComputeScoreBlackjackReturn21() {
-        Hand hand = new Hand();
         hand.draw(createSpadesCard(Rank.ACE));
         hand.draw(createSpadesCard(Rank.KING));
-        Evaluator evaluator = new Evaluator();
         Assert.assertEquals(21, evaluator.computeScore(hand));
     }
     
     @Test
     public void testComputeScoreFaceAndNumberCard() {
-        Hand hand = new Hand();
         hand.draw(createSpadesCard(Rank.EIGHT));
         hand.draw(createSpadesCard(Rank.KING));
-        Evaluator evaluator = new Evaluator();
         Assert.assertEquals(18, evaluator.computeScore(hand));
     }
     
     @Test
     public void testComputeScoreAceValueIsOneWhenHandCountMoreThanTwo() {
-        Hand hand = new Hand();
         hand.draw(createSpadesCard(Rank.ACE));
         hand.draw(createSpadesCard(Rank.EIGHT));
         hand.draw(createSpadesCard(Rank.KING));
-        Evaluator evaluator = new Evaluator();
         Assert.assertEquals(19, evaluator.computeScore(hand));
     }
     
