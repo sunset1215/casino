ab363a5cca115d7860a7b1fa99fa73c1b5e8e7a9
Vivian Low <vivian198912@gmail.com>
2018-02-25 21:19:19 +0800

    EvaluatorTest - Test compute score with another hand

--- src/main/java/enums/Suit.java
+++ src/main/java/enums/Suit.java
@@ -4,5 +4,5 @@ public enum Suit {
     DIAMONDS,
     CLUBS,
     HEARTS,
-    SPADE
+    SPADES
 }
--- src/test/java/blackjack/EvaluatorTest.java
+++ src/test/java/blackjack/EvaluatorTest.java
@@ -1,15 +1,32 @@
 package blackjack;
 
+import enums.Rank;
+import enums.Suit;
 import org.junit.Assert;
 import org.junit.Test;
-import utils.HandFactory;
 
 public class EvaluatorTest {
     
     @Test
     public void testComputeScoreBlackjackReturn21() {
-        Hand bj = HandFactory.createHandWithBlackjack();
+        Hand hand = new Hand();
+        hand.draw(createSpadesCard(Rank.ACE));
+        hand.draw(createSpadesCard(Rank.KING));
         Evaluator evaluator = new Evaluator();
-        Assert.assertEquals(21, evaluator.computeScore(bj));
+        Assert.assertEquals(21, evaluator.computeScore(hand));
+    }
+    
+    @Test
+    public void testComputeScoreFaceAndNumberCard() {
+        Hand hand = new Hand();
+        hand.draw(createSpadesCard(Rank.EIGHT));
+        hand.draw(createSpadesCard(Rank.KING));
+        Evaluator evaluator = new Evaluator();
+        Assert.assertEquals(18, evaluator.computeScore(hand));
+        
+    }
+    
+    private Card createSpadesCard(Rank rank) {
+        return new Card(Suit.SPADES, rank);
     }
 }
